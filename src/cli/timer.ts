a25f42dc175ce9ef962cbb7c74b36f05a80f89c7
syntachaosx <chaosdog.tl@gmail.com>
2021-04-18 10:39:12 +0800

    :sparkles: Timer callback

--- src/cli/timer.ts
+++ src/cli/timer.ts
@@ -4,9 +4,18 @@ let activeTimer: Timer[] = []
 let intervalFlag: NodeJS.Timeout
 
 class Timer {
-  constructor (public endTime: number, public message: string) {
+  constructor (public endTime: number, public message: string, private cb?: Function) {
     this.endTime = endTime
     this.message = message
+    this.cb = cb
+  }
+
+  done () {
+    notifier.notify({
+      title: 'loop alert',
+      message: this.message
+    })
+    this.cb && this.cb()
   }
 }
 
@@ -14,18 +23,15 @@ const checkEndTimter = () => {
   const now = Date.now()
   activeTimer = activeTimer.filter(timer => {
     if (timer.endTime < now) {
-      notifier.notify({
-        title: 'loop alert',
-        message: timer.message
-      })
+      timer.done()
     }
     return timer.endTime > now
   })
   if (activeTimer.length === 0) clearInterval(intervalFlag)
 }
 
-export const start = (time: number, message: string) => {
-  activeTimer.push(new Timer(Date.now() + time * 60 * 1000, message))
+export const start = (time: number, message: string, cb?: Function) => {
+  activeTimer.push(new Timer(Date.now() + time * 60 * 1000, message, cb))
   intervalFlag = setInterval(checkEndTimter, 1000)
 }
 
