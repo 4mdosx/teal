97c74c074c71eaf54dc1917416ecda05a4979cbb
syntachaosx <chaosdog.tl@gmail.com>
2021-09-20 20:30:15 +0800

    :art:

--- core/src/components/TaskProgress.vue
+++ core/src/components/TaskProgress.vue
@@ -4,13 +4,12 @@
     .clear.px-2(@click="cancel") X
     .name.pr-2 {{ taskConfig.name }}
     .end_at
-      .tooltipped.tooltipped-nw(:aria-label="'compete at ' + format(task.end_at, 'HH:mm:ss')") {{ process }}
+      .tooltipped.tooltipped-nw(:aria-label="'compete at ' + format(task.end_at, 'HH:mm:ss')") {{ task.process }}
 </template>
 
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
-import tasksJSON from '../../../data/tasks.json'
 import { format } from 'date-fns'
 
 export default {
@@ -29,30 +28,14 @@ export default {
   },
   computed: {
     taskConfig () {
-      return tasksJSON.find(t => t.id === this.task.id)
-    }
-  },
-  watch: {
-    task () {
-      this.updateProcess()
+      return this.store.state.game.db.get('task', this.task.id)
     }
   },
   methods: {
     format,
     cancel () {
       this.store.state.game.callEvent({ name: 'CANCEL_TASK' })
-      console.log('okk')
-    },
-    updateProcess () {
-      if (!this.task) return
-      const length = this.taskConfig.length
-      const done = Math.ceil((this.task.end_at - Date.now()) / 1000 / 60)
-      this.process = `${length - done}/${length}`
-      window.setTimeout(this.updateProcess, 10 * 1000)
     }
-  },
-  mounted () {
-    this.updateProcess()
   }
 }
 </script>
